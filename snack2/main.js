4b8af020c3f8ac234c815aa69b2dd0d4dcaeaa29
LucaRanieri96 <luca.ranieri.4@gmail.com>
2023-03-23 12:18:11 +0100

    create array and formatted array with map

---
+++ snack2/main.js
@@ -0,0 +1,15 @@
+/*
+Snack 2
+
+A partire da un array di stringhe, crea un secondo array formattando le stringhe del primo array in minuscolo e con l’iniziale maiuscola.
+
+Es: [‘pippo’, ‘PLUTO’, ‘Paperino’] => [‘Pippo’, ‘Pluto’, ‘Paperino’]
+*/
+
+// mi creo un array di stringe
+const stringArray = ['pippo', 'PLUTO', 'Paperino','toPoLINO', 'MiNNie'];
+
+// uso il metodo map per crearmi un array formattato
+const formattedArray = stringArray.map(string => {
+    
+})
