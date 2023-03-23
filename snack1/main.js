2efc87c2fbc5b0426acaf2a9ac6a2ac3faf7dc12
LucaRanieri96 <luca.ranieri.4@gmail.com>
2023-03-23 11:52:43 +0100

    also log "automobili"

--- snack1/main.js
+++ snack1/main.js
@@ -1,4 +1,3 @@
-
 /*
 Snack 1
 Crea un array composto da 10 automobili.
@@ -9,60 +8,61 @@ Dividi le automobili in 3 array separati: nel primo array solo le auto a benzina
 Infine stampa separatamente i 3 array. 
 */
 
-
 // creo un array con le 10 automobili
 const automobili = [
-    {
-      marca: "Fiat",
-      modello: "500",
-      alimentazione: "benzina"
-    },
-    {
-      marca: "Volkswagen",
-      modello: "Golf",
-      alimentazione: "diesel"
-    },
-    {
-      marca: "Renault",
-      modello: "Clio",
-      alimentazione: "benzina"
-    },
-    {
-      marca: "Tesla",
-      modello: "Model S",
-      alimentazione: "elettrico"
-    },
-    {
-      marca: "BMW",
-      modello: "i8",
-      alimentazione: "elettrico"
-    },
-    {
-      marca: "Toyota",
-      modello: "Yaris",
-      alimentazione: "ibrida"
-    },
-    {
-      marca: "Audi",
-      modello: "A3",
-      alimentazione: "gpl"
-    },
-    {
-      marca: "Mercedes-Benz",
-      modello: "GLC",
-      alimentazione: "diesel"
-    },
-    {
-      marca: "Volvo",
-      modello: "XC90",
-      alimentazione: "metano"
-    },
-    {
-      marca: "Peugeot",
-      modello: "208",
-      alimentazione: "benzina"
-    }
-  ];
+  {
+    marca: "Fiat",
+    modello: "500",
+    alimentazione: "benzina",
+  },
+  {
+    marca: "Volkswagen",
+    modello: "Golf",
+    alimentazione: "diesel",
+  },
+  {
+    marca: "Renault",
+    modello: "Clio",
+    alimentazione: "benzina",
+  },
+  {
+    marca: "Tesla",
+    modello: "Model S",
+    alimentazione: "elettrico",
+  },
+  {
+    marca: "BMW",
+    modello: "i8",
+    alimentazione: "elettrico",
+  },
+  {
+    marca: "Toyota",
+    modello: "Yaris",
+    alimentazione: "ibrida",
+  },
+  {
+    marca: "Audi",
+    modello: "A3",
+    alimentazione: "gpl",
+  },
+  {
+    marca: "Mercedes-Benz",
+    modello: "GLC",
+    alimentazione: "diesel",
+  },
+  {
+    marca: "Volvo",
+    modello: "XC90",
+    alimentazione: "metano",
+  },
+  {
+    marca: "Peugeot",
+    modello: "208",
+    alimentazione: "benzina",
+  },
+];
+
+console.log(automobili);
 
 //   ora devo dividere le automobili in base alla loro alimentazione
 
@@ -70,17 +70,16 @@ const benzina = [];
 const elettrico = [];
 const altro = [];
 
-automobili.forEach(car => {
-    if (car.alimentazione === "benzina") {
-      benzina.push(car);
-    } else if (car.alimentazione === "elettrico") {
-      elettrico.push(car);
-    } else {
-      altro.push(car);
-    }
-  });
+automobili.forEach((car) => {
+  if (car.alimentazione === "benzina") {
+    benzina.push(car);
+  } else if (car.alimentazione === "elettrico") {
+    elettrico.push(car);
+  } else {
+    altro.push(car);
+  }
+});
 
 console.log(benzina);
 console.log(elettrico);
 console.log(altro);
-
