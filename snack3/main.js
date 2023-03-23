8dce63204dc0e9d31dfb025a41ba17c99ba3740f
LucaRanieri96 <luca.ranieri.4@gmail.com>
2023-03-23 13:01:12 +0100

    update array

--- snack3/main.js
+++ snack3/main.js
@@ -12,15 +12,44 @@ Crea un nuovo array con la lista dei mammiferi.
 */
 
 const animals = [
-    { nome: 'leone', famiglia: 'felidi', classe: 'mammiferi' },
-    { nome: 'cane', famiglia: 'canidi', classe: 'mammiferi' },
-    { nome: 'gallina', famiglia: 'fasianidi', classe: 'uccelli' },
-    { nome: 't-rex', famiglia: 'sauri', classe: 'rettili' },
-    { nome: 'polpo', famiglia: 'daisjd', classe: 'invertebrati' },
-  ];
+  {
+    nome: "Leone",
+    famiglia: "Felidi",
+    classe: "Mammiferi",
+  },
+  {
+    nome: "Aquila",
+    famiglia: "Accipitridi",
+    classe: "Uccelli",
+  },
+  {
+    nome: "Squalo",
+    famiglia: "Squalidae",
+    classe: "Pesci",
+  },
+  {
+    nome: "Rana",
+    famiglia: "Ranidae",
+    classe: "Anfibi",
+  },
+  {
+    nome: "Cane",
+    famiglia: "canidi",
+    classe: "Mammiferi",
+  },
+  {
+    nome: "Orso",
+    famiglia: "Ursidae",
+    classe: "Mammiferi",
+  },
+  {
+    nome: "gallina",
+    famiglia: "fasianidi",
+    classe: "uccelli",
+  },
+];
 // come il primo esercizio ma stavolta uso filter come fabio
 // e mettiamo tutto in un unica riga tanto non abbiamo condizioni
-  const mammiferi = animals.filter(animal => animal.classe === "Mammiferi");
+const mammiferi = animals.filter((animal) => animal.classe === "Mammiferi");
 
 console.log(mammiferi);
-
